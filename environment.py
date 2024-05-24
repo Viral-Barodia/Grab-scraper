62cde575c5b26b2c8a37e093cf855572024e62ae
Viral-Barodia <202001007@daiict.ac.in>
2024-05-25 02:14:14 +0530

    environment file created

---
+++ environment.py
@@ -0,0 +1,2 @@
+driver_path = "D:\\Personal_Projects\\Anakin\\chromedriver-win64\\chromedriver.exe"
+proxy = ""
--- main.py
+++ main.py
@@ -3,20 +3,18 @@ from selenium import webdriver
 from scraper.grab_scraper import GrabScraper
 from scraper.data_processor import DataProcessor
 from threading import Thread
+from environment import driver_path, proxy
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    driver_path = "D:\\Personal_Projects\\Anakin\\chromedriver-win64\\chromedriver.exe"
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
     
-    proxy = ""
     website = "https://food.grab.com/sg/en/restaurants"
     output_file = 'restaurants.ndjson'
-    compressed_file = 'restaurants.ndjson.gz'
 
     scraper = GrabScraper(driver_path, options, proxy)
     
